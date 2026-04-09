08d19fd99db274e1160e3550a2b4db189fcbfd46
dylanpr2004 <a.dylang2004@gmail.com>
2026-04-09 07:20:10 -0400

    Servir archivos estaticos

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const io = new Server(servidor, {
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(__dirname));
 // Ruta de prueba
 app.get('/', (req, res) => {
     res.send('¡Servidor de Shopseguro funcionando!');
